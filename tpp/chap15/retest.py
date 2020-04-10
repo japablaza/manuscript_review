e691181c71c92b8a5966b1f9eed337db06c5746d
Jose Apablaza <japablaza@gmail.com>
2020-04-10 16:00:51 -0500

    Chapter 16 DONE

--- tpp/chap15/retest.py
+++ tpp/chap15/retest.py
@@ -7,8 +7,15 @@ text = 'Padre, padre, donde vas?'
 
 test_list = text.split()
 
-print(test_list)
+# print(test_list)
 
 test_re = re.split('(\W+)', text)
 
-print(test_re)
+# print(test_re)
+
+t = text.split()
+# print(t)
+x = re.split("([a-zA-Z](?:[a-zA-Z']*[a-zA-Z])?)", "Don't worry, spiders,")
+l = re.split("[a-zA-Z](?:[a-zA-Z']*[a-zA-Z])?", "Don't worry, spiders,")
+print(l)
+print(x)
